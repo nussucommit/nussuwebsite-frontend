25932254892fe86fc14a4ecda275d24251a2b586
LouisLouis19 <andreaslouis.louis@gmail.com>
2023-03-04 19:16:56 +0800

    Set up routing using react router dom

---
+++ src/constants/routes.ts
@@ -0,0 +1,13 @@
+export enum Routes {
+  index = "/",
+  aboutUs = "/aboutus",
+  governance = "/aboutus/governance",
+  history = "/aboutus/history",
+  ourTeam = "/aboutus/ourteam",
+  presidentMessage = "/aboutus/president-message",
+  contact = "/contact",
+  events = "/events",
+  freshmen = "/freshmen",
+  ourWork = "/our-work",
+  services = "/services",
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,37 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes as ROUTES } from "../constants/routes.ts";
+
+import AboutUs from "./AboutUs/AboutUsPage";
+import Governance from "./AboutUs/Governance";
+import History from "./AboutUs/History";
+import OurTeam from "./AboutUs/OurTeam";
+import PresidentMessage from "./AboutUs/PresidentMessage";
+import Contact from "./Contact";
+import Events from "./Events";
+import Freshmen from "./Freshmen";
+import Home from "./Home";
+import Ourwork from "./OurWork";
+import Service from "./Service";
+
+const Pages = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={ROUTES.index} element={<Home />} />
+        <Route path="*" element={<Navigate to={ROUTES.index} replace />} />
+        <Route path={ROUTES.aboutUs} element={<AboutUs />} />
+        <Route path={ROUTES.governance} element={<Governance />} />
+        <Route path={ROUTES.history} element={<History />} />
+        <Route path={ROUTES.ourTeam} element={<OurTeam />} />
+        <Route path={ROUTES.presidentMessage} element={<PresidentMessage />} />
+        <Route path={ROUTES.contact} element={<Contact />} />
+        <Route path={ROUTES.events} element={<Events />} />
+        <Route path={ROUTES.freshmen} element={<Freshmen />} />
+        <Route path={ROUTES.ourWork} element={<Ourwork />} />
+        <Route path={ROUTES.services} element={<Service />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Pages;
