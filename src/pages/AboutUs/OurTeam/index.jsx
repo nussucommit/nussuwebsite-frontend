f19f4e9cea6aa26a6c931192394820c3a02455ed
LouisLouis19 <andreaslouis.louis@gmail.com>
2023-04-08 10:11:47 +0800

    Create our team page

--- src/pages/AboutUs/OurTeam/index.jsx
+++ src/pages/AboutUs/OurTeam/index.jsx
@@ -1,9 +1,48 @@
 import styles from "./ourteam.module.css";
+import Navbar from "../../Navbar";
+import Footer from "../../Footer";
 
 const OurTeam = () => {
   return (
     <div className={styles.OurTeam}>
-      <p>Our Team Page</p>
+      <Navbar />
+      <div className={styles.body}>
+        <div className={styles.header}>
+          <span className={styles.odd}>Executive </span>
+          <span className={styles.even}>Committee </span>
+          <span className={styles.odd}>Officers</span>
+        </div>
+        <div className={styles.teamgrid}>
+          <div className={styles.member}>
+            <img className={styles.image} />
+            <p className={styles.name}>Lee Yat Bun</p>
+            <p className={styles.position}>President</p>
+            <p className={styles.committee}>
+              NUSSU Computing Club EXCO Representative
+            </p>
+            <p className={styles.email}>president@nussu.com</p>
+          </div>
+          <div className={styles.member}>
+            <img className={styles.image} />
+            <p className={styles.name}>Lee Yat Bun</p>
+            <p className={styles.position}>President</p>
+            <p className={styles.committee}>
+              NUSSU Computing Club EXCO Representative
+            </p>
+            <p className={styles.email}>president@nussu.com</p>
+          </div>
+          <div className={styles.member}>
+            <img className={styles.image} />
+            <p className={styles.name}>Lee Yat Bun</p>
+            <p className={styles.position}>President</p>
+            <p className={styles.committee}>
+              NUSSU Computing Club EXCO Representative
+            </p>
+            <p className={styles.email}>president@nussu.com</p>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
