12f0c7af590419695add6f977c59a9e7e15ce204
LouisLouis19 <andreaslouis.louis@gmail.com>
2023-08-27 22:17:04 +0800

    Create homepage with backend integration

---
+++ src/pages/Home/helper.jsx
@@ -0,0 +1,78 @@
+export const extractAnnouncement = (data) => {
+  let extractedText = "";
+  for (const item of data) {
+    if (item.type === "paragraph" && item.content.length > 0) {
+      const textContent = item.content[0];
+      if (textContent.type === "text" && textContent.content) {
+        extractedText = textContent.content;
+        break; // Stop processing once the text is extracted
+      }
+    }
+  }
+
+  return extractedText;
+};
+
+export const extractBannerImages = (data) => {
+  const bannerImages = [];
+
+  let foundBannerHeading = false;
+
+  for (const item of data) {
+    if (item.type === "heading") {
+      if (item.content === "Banner") {
+        foundBannerHeading = true;
+      } else {
+        foundBannerHeading = false;
+      }
+    } else if (foundBannerHeading && item.type === "image") {
+      bannerImages.push({
+        url: item.content,
+      });
+    }
+  }
+
+  return bannerImages;
+};
+
+export const extractEventsImages = (data) => {
+  const eventsImages = [];
+
+  let foundEventsHeading = false;
+
+  for (const item of data) {
+    if (item.type === "heading") {
+      if (item.content === "Slideshow") {
+        foundEventsHeading = true;
+      } else {
+        foundEventsHeading = false;
+      }
+    } else if (foundEventsHeading && item.type === "image") {
+      eventsImages.push({
+        url: item.content,
+      });
+    }
+  }
+
+  return eventsImages;
+};
+
+export const extractInstagramUrls = (data) => {
+  const instagramUrls = [];
+
+  data.forEach((item) => {
+    if (item.type === "paragraph") {
+      item.content.forEach((contentItem) => {
+        if (
+          contentItem.type === "text" &&
+          contentItem.attribute &&
+          contentItem.attribute.link
+        ) {
+          instagramUrls.push(contentItem.content);
+        }
+      });
+    }
+  });
+
+  return instagramUrls;
+};
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,67 @@
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 import styles from "./home.module.css";
+import SimpleImageSlider from "react-simple-image-slider";
+import { useFetchData } from "../../common/useFetchData";
+import { Routes } from "../../constants/routes";
+import logo from "../../assets/nussu_logo.png";
+import {
+  extractAnnouncement,
+  extractBannerImages,
+  extractEventsImages,
+  extractInstagramUrls,
+} from "./helper";
+import { InstagramEmbed } from "react-social-media-embed";
 
 const Home = () => {
+  const url = Routes.backendRoot + Routes.home;
+  const [isLoading, content] = useFetchData(url);
+  const announcement = extractAnnouncement(content);
+  const bannerImages = extractBannerImages(content);
+  const eventsImages = extractEventsImages(content);
+  const instagramUrls = extractInstagramUrls(content);
+  const width = 1500;
+  const height = 750;
   return (
     <div className={styles.Home}>
       <Navbar />
-      <p>Home Page</p>
+      <div className={styles.announcement}>{announcement}</div>
+      <div className={styles.header}>
+        <p className={styles.quote}>
+          We <span style={{ color: "#0177CC", fontWeight: "bold" }}>work</span>{" "}
+          for students!
+        </p>
+        <img className={styles.image} src={logo} alt="NUSSU logo"></img>
+      </div>
+      <div className={styles.banner}>
+        <SimpleImageSlider
+          className={styles.slider}
+          width={width}
+          height={height}
+          images={bannerImages}
+          showNavs={true}
+          autoPlay={true}
+        />
+      </div>
+      <div className={styles.events}>
+        <p className={styles.eventsheader}>Events</p>
+        <SimpleImageSlider
+          className={styles.slider}
+          width={width}
+          height={height}
+          images={eventsImages}
+          showNavs={true}
+        />
+      </div>
+      <div className={styles.socialheader}>
+        <span className={styles.odd}>Follow </span>
+        <span className={styles.even}>Us</span>
+      </div>
+      <div className={styles.container}>
+        {instagramUrls.map((url, index) => (
+          <InstagramEmbed url={url} width={350} height={"50vh"} />
+        ))}
+      </div>
       <Footer />
     </div>
   );
