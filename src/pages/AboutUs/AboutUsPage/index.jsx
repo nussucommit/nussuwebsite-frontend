abd5bba842d1ce3199dc14d5e113545262582c4a
Daphne789 <daphneshaine933@gmail.com>
2024-01-03 18:33:22 +0800

    Completion of About Us page

--- src/pages/AboutUs/AboutUsPage/index.jsx
+++ src/pages/AboutUs/AboutUsPage/index.jsx
@@ -1,13 +1,51 @@
-import styles from "./aboutus.module.css";
-import Maintenance from "../../Maintenance";
+import React from "react";
 import Navbar from "../../Navbar";
 import Footer from "../../Footer";
+import styles from "./aboutus.module.css";
+import { useFetchData } from "../../../common/useFetchData";
+import { Routes } from "../../../constants/routes";
+import {
+  extractImage,
+  extractParagraphs,
+  extractVision,
+  extractMission,
+} from "./helper";
 
 const AboutUs = () => {
+  const url = Routes.backendRoot + Routes.aboutUs;
+  const [isLoading, content] = useFetchData(url);
+  const image = extractImage(content);
+  const paragraphs = extractParagraphs(content);
+  const vision = extractVision(content);
+  const mission = extractMission(content);
+
   return (
-    <div className={styles.AboutUs}>
+    <div>
       <Navbar />
-      <Maintenance />
+      <div className={styles.header}></div>
+      <hr className={styles.horizontalLine} />
+      <div className={styles.container}>
+        <div className={styles.leftcolumn}>
+          <img src={image} className={styles.image} alt="Group Photo" />
+          <div>
+            <div className={styles.visionheader}></div>
+            <hr className={styles.horizontalLine2} />
+            <div className={styles.visioncontent}>{vision.content}</div>
+          </div>
+          <div>
+            <div className={styles.missionheader}></div>
+            <hr className={styles.horizontalLine2} />
+            <div className={styles.missioncontent}>{mission.content}</div>
+          </div>
+        </div>
+        <div className={styles.rightcolumn}>
+          <div className={styles.maincontent}>
+            {paragraphs.map((paragraph, index) => (
+              <p key={index}>{paragraph}</p>
+            ))}
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
