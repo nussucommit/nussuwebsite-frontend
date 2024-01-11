f35199a1e91df4b021dc61d3e2f9a2d0a17140ba
wjacobw <williamjacob0910@gmail.com>
2024-01-11 16:29:31 +0800

    display loading page when fetching data

---
+++ src/pages/Freshmen/faqcard/FaqCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./faqcard.module.css";
+
+const FaqCard = (props) => {
+    const question = props.question;
+    const answer = props.answer;
+    const index = props.index
+
+    return (
+      <div className={styles.faqcard}>
+        <div className={styles.question}>{index + `. `}{question}</div>
+        <div className={styles.answer}>{answer}</div>
+      </div>
+    );
+  };
+
+  export default FaqCard;
--- src/pages/Freshmen/index.jsx
+++ src/pages/Freshmen/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./freshmen.module.css";
-import Maintenance from "../Maintenance";
+
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 import { useFetchData } from "../../common/useFetchData";
 import { Routes } from "../../constants/routes";
+import EventCard from "./eventcard/EventCard";
+
 import {
   extractFAQ,
   extractFOP,
@@ -15,6 +17,7 @@ import {
   extractFOPNUS,
   extractFOPTypeDesc,
 } from "./helper";
+import FaqCard from "./faqcard/FaqCard";
 
 const Freshmen = () => {
   const url = Routes.backendRoot + Routes.freshmen;
@@ -28,6 +31,27 @@ const Freshmen = () => {
   const FOPNUS = extractFOPNUS(content);
   const FOPTypeDesc = extractFOPTypeDesc(content);
 
+  function formatString(str) {
+    const parts = str.split(" "); // This splits the string into words
+    return (
+      <>
+        <span style={{ color: "#d9a129" }}>{parts[0]}</span>{" "}
+        {" " + parts.slice(1).join(" ")}
+      </>
+    );
+  }
+
+  function removeInitialCommaSpace(text) {
+    if (text.startsWith(", ")) {
+      return text.substring(2);
+    }
+    return text;
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className={styles.Freshmen}>
       <Navbar />
@@ -35,12 +59,32 @@ const Freshmen = () => {
         <span>{FOP[0]}</span>
       </div>
       <hr className={styles.horizontalLine} />
-      <div className={styles.title}>{FOP[1]}</div>
+      <div className={styles.title}>{formatString(FOP[1])}</div>
       <hr className={styles.horizontalLine} />
       <div className={styles.content}>{FOPDesc} </div>
-      <div className={styles.title}>{FOPNUS}</div>
+      <div className={styles.yellowtitle}>{FOPNUS}</div>
       <hr className={styles.horizontalLine} />
       <div className={styles.content}>{FOPTypeDesc} </div>
+      <div className={styles.programsection}>
+        {CampTypes.map((type) => (
+          <EventCard
+            eventName={type[0]}
+            eventDesc={removeInitialCommaSpace(type[1])}
+          ></EventCard>
+        ))}
+      </div>
+      <p className={styles.content}>
+        {FOPCategory[0][0]}
+        <a href={FOPCategory[1]} className={styles.underlineLink}>
+          {FOPCategory[0][1]}
+        </a>
+        {FOPCategory[0][2]}
+      </p>
+      <div className={styles.yellowtitle}>{FAQ[0]}</div>
+      <hr className={styles.horizontalLine} />
+      {FAQ[1].map((faq, index) => (
+        <FaqCard answer={faq[1]} question={faq[0]} index={index + 1}></FaqCard>
+      ))}
 
       <Footer />
     </div>
