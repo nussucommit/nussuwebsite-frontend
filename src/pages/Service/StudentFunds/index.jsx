e64567593d99632210d4b937aa5fb14fd7674afb
LouisLouis19 <andreaslouis.louis@gmail.com>
2024-06-09 21:14:43 +0700

    Students' Fund Page CSS

--- src/pages/Service/StudentFunds/index.jsx
+++ src/pages/Service/StudentFunds/index.jsx
@@ -15,6 +15,10 @@ import {
   extractTermsAndConditions,
 } from './helper';
 
+import CIcon from '@coreui/icons-react';
+import { freeSet } from '@coreui/icons'
+// import { cilFilePdf } from '@coreui/icons';
+
 
 const StudentFunds = () => {
     const url = Routes.backendRoot + Routes.studentfunds;
@@ -27,52 +31,69 @@ const StudentFunds = () => {
     const applicationPeriod = extractApplicationPeriod(content);
     const resultsRelease = extractResultsRelease(content);
     const applicationLink = extractApplicationLink(content);
-    const termsAndConditions = extractTermsAndConditions(content);
-    console.log(termsAndConditions);
+    const termsAndConditions = "https://prod-files-secure.s3.us-west-2.amazonaws.com/58192aff-311a-42ce-88ef-f7a68a1798f3/e89acd53-73c8-4c6c-9e4f-7eca5eb47157/NUSSU-Students--Funds-Terms---Conditions.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45HZZMZUHI%2F20240609%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20240609T141121Z&X-Amz-Expires=3600&X-Amz-Signature=ccf797c03682bb7f9a16c5e88853309e63aa507b7dd45399cd766f9c3ac8ec21&X-Amz-SignedHeaders=host&x-id=GetObject";
 
     return (
         <div>
             <Navbar />
-            <h1>NUSSU Students' Fund</h1>
-            <div className={styles.image}>
-                <img src={image} />
+            <h1 className={styles.header}>NUSSU Students' Fund</h1>
+            <div className={styles.imageContainer}>
+                <img src={image} className={styles.image}/>
             </div>
 
-            <div className={styles.about}>
-                <h2>ABOUT NUSSU Students' Fund</h2>
-                <p>{about}</p>
+            <div className={styles.content}>
+                <div className={styles.about}>
+                    <h2 className={styles.subheader}>ABOUT NUSSU Students' Fund</h2>
+                    <hr className={styles.horizontalLine} />
+                    <p className={styles.text}>{about}</p>
+                </div>
+
+                <div className={styles.lateApplications}>
+                    <h2 className={styles.subheader}>Late Applications</h2>
+                    <hr className={styles.horizontalLine} />
+                    <p className={styles.text}>{lateApplications}</p>
+                </div>
+
+                <div className={styles.eligibility}>
+                    <h2 className={styles.subheader}>Eligibility</h2>
+                    <hr className={styles.horizontalLine} />
+
+                    
+                    <p className={styles.text}>{eligibility[0]}</p>
+                    <ul className={styles.text}>
+                        {eligibility.slice(1).map((item, index) => (
+                            <li key={index}>{item}</li>
+                        ))}
+                    </ul>
+                    
+                </div>
+
+                {/* <div className={styles.assistance}>
+                    <h2 className={styles.subheader}>Assistance Provided</h2>
+                    <p className={styles.text}> {assistanceProviced}</p>
+                </div> */}
+
+                <div className={styles.application}>
+                    <h2 className={styles.subheader}>Application</h2>
+                    <hr className={styles.horizontalLine} />
+                    <h3 className={styles.textheader}>Application Period</h3>
+                    <p className={styles.text}>{applicationPeriod}</p>
+                    <h3 className={styles.textheader}>Release of Results</h3>
+                    <p className={styles.text}>{resultsRelease}</p>
+                    <h3 className={styles.textheader}>Application Link</h3>
+                    <p className={styles.text}>{applicationLink}</p>
+                </div>
+
+                <div className={styles.terms}>
+                    <h2 className={styles.subheader}>Terms and Conditions</h2>
+                    <hr className={styles.horizontalLine} />
+                    {/* <div>{termsAndConditions}</div> */}
+                    <a href={termsAndConditions} target="_blank" rel="noopener noreferrer" style={styles.link}>
+                        <CIcon icon={freeSet.cilFile} height="10vh" width="7.5vw" />
+                    </a>
+                </div>
             </div>
-
-            <div className={styles.lateApplications}>
-                <h2>Late Applications</h2>
-                <p>{lateApplications}</p>
-            </div>
-
-            <div className={styles.eligibility}>
-                <h2>Eligibility</h2>
-                <p>{eligibility}</p>
-            </div>
-
-            {/* <div className={styles.assistance}>
-                <h2>Assistance Provided</h2>
-                <p>{assistanceProviced}</p>
-            </div> */}
-
-            <div className={styles.application}>
-                <h2>Application</h2>
-                <h3>Application Period</h3>
-                <p>{applicationPeriod}</p>
-                <h3>Release of Results</h3>
-                <p>{resultsRelease}</p>
-                <h3>Application Link</h3>
-                <p>{applicationLink}</p>
-            </div>
-
-            <div className={styles.terms}>
-                <h2>Terms and Conditions</h2>
-                <div>{termsAndConditions}</div>
-            </div>
-
+        
             <Footer />
         </div>
    )
